a9f100f1d631f4048b60a58f4d779ae6c8878c2a
onigirikk <122284491+onigirikk@users.noreply.github.com>
2023-01-17 14:39:56 +0000

    [Finish]2-9

--- chapter9/new.rb
+++ chapter9/new.rb
@@ -1,4 +1,10 @@
 class Car
+  
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+  
   def turn(direction)
     puts "#{direction}に曲がります。"
   end
@@ -9,5 +15,20 @@ class Car
 end
 
 car = Car.new
-car.turn("右")
-car.run(5)
+car.move("右",5)
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+Car.run(10)
+
+class Car
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
